48575c831ebdb14551f267a4f15f33035b642f51
aimee attenborough <aimeelattenborough@gmail.com>
2018-02-10 17:43:42 +0000

    refactoring code for legibility, adding a playGame function

--- src/js/app.js
+++ src/js/app.js
@@ -17,41 +17,40 @@ $(() => {
   // VARIABLES
   const $timerText = $('#timerText');
   console.log($timerText);
+  const $gameplayArea = $('.gameplayArea');
+  let wordsOnScreen = [];
+  const wordList = ['banana', 'pineapple', 'chihuahua', 'robot', 'noodle', 'chicken', 'turtle'];
 
   // PLAY GAME FUNCTION
-  // function playGame() {
-  // timer();
-  //
-  // }
+  function playGame() {
+    timer();
+
+  }
 
   // TIMER
-  // function timer() {
-  //     let time = 30;
-  //     let timeRunning = false;
-  //     let countDown = null;
-  //     timeRunning = true;
-  //     if (timeRunning === true) {
-  //       countDown = setInterval(() => {
-  //         time--;
-  //         if (time === 0) {
-  //           clearInterval(countDown);
-  //           timeRunning = false;
-  //         }
-  //         $timerText.html(time);
-  //       }, 1000);
-  //     }
-  //   }
+  function timer() {
+    let time = 30;
+    let timeRunning = false;
+    let countDown = null;
+    timeRunning = true;
+    if (timeRunning === true) {
+      countDown = setInterval(() => {
+        time--;
+        if (time === 0) {
+          clearInterval(countDown);
+          timeRunning = false;
+        }
+        $timerText.html(time);
+      }, 1000);
+    }
+  }
 
-  // CREATE WORDS
-  const $gameplayArea = $('.gameplayArea');
-  const wordsOnScreen = [];
-  const wordList = ['banana', 'pineapple', 'chihuahua'];
+  // WORD PLAY
 
-
-// setInterval is self calling eveery second and invoking the runCreateWords function
+  // setInterval is self calling eveery second and invoking the runCreateWords function
   setInterval(() => {
     runCreateWords();
-  }, 1000);
+  }, 2000);
 
   // function runCreateWords is generating a random number which is being apssed into the timeout function. The timeout is calling the createWords function at a random interval passing  in rand as an argument.
   function runCreateWords() {
@@ -60,71 +59,49 @@ $(() => {
       createWords();
     }, rand);
   }
-  // removeWord is removing the specific element with the same key id as that which has been generated in createWords, and shift is removing the first word from the wordsOnScreen array.
-
-  // NEED TO REMOVE VIA OBJECT KEY WORDS ON SCREEN !!!! *********
-  function removeWord(key) {
-    setTimeout(() => {
-      $('#' + key).remove();
-      console.log(wordsOnScreen);
-      wordsOnScreen.shift();
-    }, 5000);
-  }
-// //
-// function removeWordFromArray(wordsOnScreen, key) {
-//   wordsOnScreen.some(function(randomWord, key) {
-//     return(wordsOnScreen[key])
-//   })
-//
-// }
-//
-// var items = [
-//   { id: 1 },
-//   { id: 3 },
-//   { id: 1111 },
-//   { id: 43 },
-//   { id: 67 },
-//   { id: 12 },
-// ];
-//
-// function removeByKey(array, params){
-//   array.some(function(item, index) {
-//     return (array[index][params.key] === params.value) ? !!(array.splice(index, 1)) : false;
-//   });
-//   return array;
-// }
-//
-// var removed = removeByKey(items, {
-//   key: 'id',
-//   value: 43
-// });
-//
-// console.log(removed);
-//
-//
-
-
-
   // createWords is creating a random word from an array and pushing it into an empty array. The const key is generating a random interger which is being passed in as the id of the random word generated, allowing the specific word to be removed when the removeWord function is called.
   function createWords(){
     const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     const key = Math.floor(Math.random() * 100);
     wordsOnScreen.push({key: key, word: randomWord});
     $gameplayArea.append(`<p id="${key}" class="wordScroll">${randomWord}</p>`);
-    removeWord(key);
+    removeWordFromScreen(key);
   }
-
-
+  // removeWord is removing the specific element with the same key id as that which has been generated in createWords, and shift is removing the first word from the wordsOnScreen array.
+  function removeWordFromScreen(key) {
+    setTimeout(() => {
+      $('#' + key).remove();
+      console.log(wordsOnScreen);
+      removeWordFromArray(key);
+    }, 5000);
+  }
+// // removeWordFromArray is filtering through the words that don't have the current key - taking the current word out..
+  function removeWordFromArray(key)  {
+    wordsOnScreen = wordsOnScreen.filter(word => word.key !== key);
+  }
 
   // CHECK FOR MATCH
+  const $form = $('.form');
+  const $input = $('#text-box');
+  const $score = $('#score');
+  let result = 0;
 
   function checkForMatch() {
-
+    $form.on('submit', (e) => {
+      e.preventDefault();
+      wordsOnScreen.find(wordInArray => {
+        console.log(wordInArray, $input.val());
+        if (wordInArray.word === $input.val()) {
+          result ++;
+          $score.text(result);
+          console.log('yay');
+        }
+      });
+    });
   }
 
+  checkForMatch();
 
 
-
-
-//DON'T DELETE End of page
+// End of page
 });
